7d44618ed43e7df261aeec82acba9230da480ae4
yoon <mono74140@gmail.com>
2024-09-11 14:59:37 +0900

    terraform AWS EC2 userdata - mariadb

---
+++ ec2_userdata_mariadb/main.tf
@@ -0,0 +1,33 @@
+# mariadb를 설치하는 user-data
+
+data "template_file" "mariadb_userdata" {
+  template = <<-EOF
+  apt-get install -y apt-transport-https curl
+  mkdir -p /etc/apt/keyrings
+  curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
+  cat <<EOL > /etc/apt/sources.list.d/mariadb.sources
+  # MariaDB 10.11 repository list - created 2024-09-10 01:09 UTC
+  # https://mariadb.org/download/
+  X-Repolib-Name: MariaDB
+  Types: deb
+  # deb.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+  # URIs: https://deb.mariadb.org/10.11/ubuntu
+  URIs: https://tw1.mirror.blendbyte.net/mariadb/repo/10.11/ubuntu
+  Suites: jammy
+  Components: main main/debug
+  Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
+  EOL
+  apt-get update
+  apt-get install -y mariadb-server
+  sudo mariadb-secure-installation << EOF
+
+  y
+  y
+  ubuntu
+  ubuntu
+  y
+  n
+  y
+  y
+  EOF
+}
